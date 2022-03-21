fa31d6b119b9ff25b91e17d0db25fb1208dc300a
Batu <gulbatu3@googlemail.com>
2022-03-21 10:37:01 +0100

    admin page UI changes

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,28 @@
-# Register your models here.
+# Admin form customization
 
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    #choices are shown inline with corresponding question
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+        ('Category', {'fields': ['question_category']}),
+    ]
+
+    inlines = [ChoiceInline]
+    #columns of displayed list of questions:
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    #additional filter for list of questions:
+    list_filter = ['pub_date']
+    #search capability:
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 
--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,14 @@
 # With these classes, django is able to create a python DB-access API
 
 import datetime
+from xmlrpc.client import boolean
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
+    #fields
     question_text = models.CharField(max_length=200)
     question_category = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
@@ -15,13 +18,21 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+
+    #change column header in admin page
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
-        return now >= self.pub_date >= now - datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
 
 
 class Choice(models.Model):
+    #fields
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
